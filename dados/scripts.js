127aebba5d1861cd772517aba51f4734eb1d1a25
morfe <tiago01cracy@gmail.com>
2023-03-14 19:21:41 -0500

    se elimino un carpeta

---
+++ dados/scripts.js
@@ -0,0 +1,32 @@
+
+let dadosContainer = document.getElementById("dados-container");
+
+const btnTirar = document.getElementById("btn-tirar");
+document.getElementById("nDados").value = 1;
+
+
+btnTirar.addEventListener("click", () => tirarDados());
+
+
+function tirarDados() {
+
+    dadosContainer.innerHTML = "";
+
+    for (let i = 0; i < document.getElementById("nDados").value; i++) {
+        setTimeout(function () {
+
+            let dado = document.createElement("img");
+            dado.classList.add("dado");
+            dadosContainer.appendChild(dado);
+            dado.src = "img/cara_0.svg";
+
+            setTimeout(function () {
+                let resultado = Math.floor(Math.random() * 6) + 1;
+                dado.src = "img/cara_" + resultado + ".svg";
+                document.documentElement.style.setProperty('--random', Math.random());
+            }, 460);
+
+        }, i * 720);
+    }
+
+}
