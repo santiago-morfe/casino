8a117e737b757ddbfe4b36ac8cf14c971579aad3
morfe <tiago01cracy@gmail.com>
2023-03-15 13:55:39 -0500

    se corrigieron errores del los dados

--- dados/scripts.js
+++ dados/scripts.js
@@ -1,37 +1,40 @@
-
 let dadosContainer = document.getElementById("dados-container");
-
 const btnTirar = document.getElementById("btn-tirar");
 document.getElementById("nDados").value = 1;
-
 
 btnTirar.addEventListener("click", () => tirarDados());
 
 
 function tirarDados() {
 
+    btnTirar.disabled = true;
+
+    let dados = document.getElementById("nDados").value;
     dadosContainer.innerHTML = "";
     dadosContainer.classList.add("shake")
+    let i = 1
 
-    for (let i = 0; i < document.getElementById("nDados").value; i++) {
-        setTimeout(function () {
-
+    setInterval(function () {
+        if (i <= dados) {
             let dado = document.createElement("img");
+            dado.src = "img/cara_0.svg";
             dado.classList.add("dado");
             dadosContainer.appendChild(dado);
-            dado.src = "img/cara_0.svg";
-
+            let resultado = Math.floor(Math.random() * 6) + 1;
             setTimeout(function () {
-                let resultado = Math.floor(Math.random() * 6) + 1;
                 dado.src = "img/cara_" + resultado + ".svg";
-                document.documentElement.style.setProperty('--random', Math.random());
             }, 460);
+        } else {
+            clearInterval(intervalo);
+        }
+        i++;
 
-        }, 1000 + (i * 950));
-    }
+    }, 1000);
+
+
     setTimeout(function () {
-        dadosContainer.classList.remove("shake")
-    },1000)
-
-
+        dadosContainer.classList.remove("shake");
+        btnTirar.disabled = false;
+    }, 1300 + (dados  * 1000))
 }
+
